51e8fc06ce409b0fc1427fdd62113c54399ea576
KimJeongtae <tae101010@naver.com>
2022-05-30 02:53:46 +0900

    add directory

---
+++ AllergyApp/Model/Allergy.swift
@@ -0,0 +1,17 @@
+//
+//  Allergy.swift
+//  AllergyApp
+//
+//  Created by 김정태 on 2022/05/30.
+//
+
+import Foundation
+
+//allergy model
+struct Allergy {
+    var id: String
+    var level: Int
+    var myAllergy: String
+    var allergyName: String
+    var highLevelAllergy: String
+}
---
+++ AllergyApp/Model/Result.swift
@@ -0,0 +1,17 @@
+//
+//  Result.swift
+//  AllergyApp
+//
+//  Created by 김정태 on 2022/05/30.
+//
+
+import Foundation
+
+//결과 model
+struct Result {
+    var id: String
+    var productName: String
+    var productContetn: String
+    var allergyResult: String
+    var createDate: String
+}
