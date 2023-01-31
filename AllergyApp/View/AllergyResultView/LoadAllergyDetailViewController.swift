77215d3c8ba99d8487904351da605a202c7eed82
js20028 <mix2088@nate.com>
2023-01-31 17:45:32 +0900

    aaa

--- AllergyApp/View/AllergyResultView/LoadAllergyDetailViewController.swift
+++ AllergyApp/View/AllergyResultView/LoadAllergyDetailViewController.swift
@@ -14,7 +14,7 @@ class LoadAllergyDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("12345")
+        print("12345123")
         
     }
 }
