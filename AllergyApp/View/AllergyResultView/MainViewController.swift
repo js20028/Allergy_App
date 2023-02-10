847a5fe88fc3294ee00a58700abfc808f43b4f60
js20028 <mix2088@nate.com>
2023-02-10 16:21:10 +0900

    merge 후 수정

--- AllergyApp/View/AllergyResultView/MainViewController.swift
+++ AllergyApp/View/AllergyResultView/MainViewController.swift
@@ -10,10 +10,24 @@ import RxSwift
 import RxCocoa
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var registerAllergyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        registerAllergyButton.rx.tap
+            .bind(onNext: {
+                let showAllergyStoryboard = UIStoryboard.init(name: "ShowAllergy", bundle: nil)
+                
+                guard let myAllergyViewController = showAllergyStoryboard.instantiateViewController(withIdentifier: "MyAllergyViewController") as? MyAllergyViewController else { return }
+                
+                self.present(myAllergyViewController, animated: false, completion: nil)
+            })
+        
     }
+    
+    
+    
 }
 
