8f3cf1e8c3a60856de3af60f6eff4ecc991896ec
tae1010 <tae101010@naver.com>
2023-02-22 17:45:04 +0900

    알러지 뷰 디자인 변경 + 팝업창 디자인

--- AllergyApp/View/AllergyView/CheckDeletePopupViewController.swift
+++ AllergyApp/View/AllergyView/CheckDeletePopupViewController.swift
@@ -7,3 +7,61 @@
 
 import Foundation
 import UIKit
+import RxCocoa
+import RxSwift
+
+enum allergyViewStatus {
+    case totalAllergy
+    case myAllergy
+}
+
+class CheckDeletePopupViewController: UIViewController {
+    
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    var allergyViewStatus: allergyViewStatus?
+    var totalAllergyViewModel: TotalAllergyViewModel?
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configureButton()
+        self.configurePopupView()
+        
+        deleteButton.rx.tap.bind(onNext: {
+            
+            if self.allergyViewStatus == .myAllergy {
+                print("마이")
+                self.totalAllergyViewModel?.tapdelete.onNext(self.totalAllergyViewModel?.checkMyAllergy.value ?? [])
+            } else {
+                print("토탈")
+                self.totalAllergyViewModel?.tapTotaldelete.onNext(self.totalAllergyViewModel?.checkAllergy.value ?? [])
+            }
+            
+            self.dismiss(animated: true)
+        }).disposed(by: disposeBag)
+        
+        
+        dismissButton.rx.tap.bind(onNext: {
+            self.dismiss(animated: true)
+        }).disposed(by: disposeBag)
+        
+        
+        
+    }
+}
+
+extension CheckDeletePopupViewController {
+    func configureButton() {
+        self.dismissButton.layer.cornerRadius = 10
+        self.deleteButton.layer.cornerRadius = 10
+    }
+    
+    func configurePopupView() {
+        self.popUpView.layer.cornerRadius = 10
+    }
+}
